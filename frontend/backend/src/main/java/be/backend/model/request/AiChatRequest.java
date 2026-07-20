6e1d6cde06ccadb7eeb4cf821c2c08c79116c756
Leo3724 <vietnxse184785@fpt.edu.vn>
2026-07-20 16:30:53 +0700

    remove backend folder from fe repo and update home UI arrows

--- frontend/backend/src/main/java/be/backend/model/request/AiChatRequest.java
+++
@@ -1,8 +0,0 @@
-package be.backend.model.request;
-
-import lombok.Data;
-
-@Data
-public class AiChatRequest {
-    private String message;
-}
--- frontend/backend/src/main/resources/database/migration/V11__alter_movie_release_date_to_datetime.sql
+++
@@ -1,5 +0,0 @@
--- ============================================================
--- V11: Alter movie release_date to DATETIME
--- ============================================================
-
-ALTER TABLE movie MODIFY COLUMN release_date DATETIME;
